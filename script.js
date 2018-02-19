4f960f879881f1e815147f5d07e6ae2c5de5aa88
silver-io <javier@silver.io>
2018-02-19 06:33:06 +0000

    hooks up both files so we can interact with the user without using the console

--- script.js
+++ script.js
@@ -59,11 +59,17 @@ var todoList = {
   }
 };
 
+//interacting with the user
 var handlers = {
   displayTodos: function() {
     todoList.displayTodos();
   },
   toggleAll: function() {
     todoList.toggleAll();
+  },
+  addTodo: function() {
+    var addTodoTextInput = document.getElementById("addTodoTextInput");
+    todoList.addTodo(addTodoTextInput.value);
+    addTodoTextInput.value = "";
   }
 };
